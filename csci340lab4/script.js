d7a19cb4c061815b5594acc2b9163d2c50d682b0
ianwalker04 <143114214+ianwalker04@users.noreply.github.com>
2023-09-08 18:34:01 -0500

    Testing

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function() {
+    $("#clicker").click(function () {
+        $.ajax({
+            dataType: "json",
+            url: "https://catboys.com/api/img",
+            success: function(results) {
+                $("#catboy").attr("src", results["url"]);
+            }
+        });
+    });
+});
