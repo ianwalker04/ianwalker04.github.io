405968894f23625ae4f8fe3c716c25a082d398ef
ianwalker04 <143114214+ianwalker04@users.noreply.github.com>
2023-09-11 13:25:09 -0500

    Script update

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+    console.log("Loaded");
     $("#clicker1").click(function() {
         $.ajax({
             dataType: "json",
             url: "https://random-d.uk/api/v2/random",
             success: function(results) {
                 $("#duck").attr("src", results["url"]);
+                console.log("Success");
+                console.log(results);
             },
             error: function(xhr,status,error) {
                 console.log(error);
