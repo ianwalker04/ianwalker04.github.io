7c3f31a1767ee5d9c04c043d03f4b21d61eb14a0
ianwalker04 <143114214+ianwalker04@users.noreply.github.com>
2023-09-11 16:26:11 -0500

    Finished Lab4

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
-    console.log("Loaded");
-    $("#clicker1").click(function() {
-        $.ajax({
-            dataType: "json",
-            url: "https://random-d.uk/api/v2/random",
-            success: function(results) {
-                $("#duck").attr("src", results["url"]);
-                console.log("Success");
-                console.log(results);
-            },
-            error: function(xhr,status,error) {
-                console.log(error);
-            }
-        });
+    $('#clicker1').click(function() {
+      $.ajax({
+        dataType: "json",
+        url: "https://random.dog/woof.json",
+        success: function(results) {
+          console.log(results["url"]);
+          if (results["url"].endsWith(".mp4")) {
+            $('#dog').attr("src", "Images/dog1.jpg");
+          } else {
+            $('#dog').attr("src", results["url"]);
+          }
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        }
+      });
     });
+  });
+
+$(document).ready(function() {
+  $('#clicker2').click(function() {
+    $.ajax({
+      dataType: "jsonp",
+      jsonpCallback: "parseQuote",
+      url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote",
+      success: function(results) {
+        $("h4").text(results["quoteText"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  });
 });
