b6b9d84fc4a1cf225274daeb8c01d3a8fd1421eb
ianwalker04 <143114214+ianwalker04@users.noreply.github.com>
2023-10-24 18:05:21 -0500

    Lab 9 Progress

--- csci340lab9/ContosoUniversity/Pages/Students/Details.cshtml.cs
+++ csci340lab9/ContosoUniversity/Pages/Students/Details.cshtml.cs
@@ -22,22 +22,23 @@ namespace ContosoUniversity.Pages.Students
       public Student Student { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null || _context.Student == null)
-            {
-                return NotFound();
-            }
+{
+    if (id == null)
+    {
+        return NotFound();
+    }
 
-            var student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Student = student;
-            }
-            return Page();
-        }
+    Student = await _context.Students
+        .Include(s => s.Enrollments)
+        .ThenInclude(e => e.Course)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(m => m.ID == id);
+
+    if (Student == null)
+    {
+        return NotFound();
+    }
+    return Page();
+}
     }
 }
--- csci340lab9/ContosoUniversity/Program.cs
+++ csci340lab9/ContosoUniversity/Program.cs
@@ -6,8 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SchoolContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("SchoolContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("SchoolContextSQLite")));
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +18,19 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+    DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
