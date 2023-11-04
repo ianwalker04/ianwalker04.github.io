446df778d98bcb51f42a1299340f77636ba3ee11
ianwalker04 <143114214+ianwalker04@users.noreply.github.com>
2023-11-04 11:17:47 -0500

    Lab 9 progress

---
+++ csci340lab9/RazorPagesWalker/WalkerUniversity/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WalkerUniversity.Data;
+using WalkerUniversity.Models;
+
+namespace WalkerUniversity.Pages.Courses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WalkerUniversity.Data.SchoolContext _context;
+
+        public DetailsModel(WalkerUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+      public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Course = course;
+            }
+            return Page();
+        }
+    }
+}
---
+++ csci340lab9/RazorPagesWalker/WalkerUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using WalkerUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WalkerUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WalkerUniversity.Data.SchoolContext _context;
+
+        public IndexModel(WalkerUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Courses { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
